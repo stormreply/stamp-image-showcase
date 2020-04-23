856ca39636634a687f0c6b0bc7221ca3c1971394
Henning Teek <h.teek@reply.de>
2020-04-24 00:28:21 +0200

    revert(stamp-image): removed s3 bucket and deployment for demo purposes

--- lib/index.ts
+++ lib/index.ts
@@ -1,28 +1,17 @@
 import * as cdk from '@aws-cdk/core';
 
-import { StampImageLamnbda, StampImageBucket, StampImageBucketDeployment } from './stamp-image';
+import { StampImageLamnbda } from './stamp-image';
 
 export class StampImageStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         // 1. Create lambda function for stamping images
-        const handler = new StampImageLamnbda(this, 'Function', {
+        new StampImageLamnbda(this, 'Function', {
             functionName: 'StampImageFunction',
             handler: 'app.handler',
             timeout: cdk.Duration.seconds(60),
             memorySize: 1024,
-        });
-
-        // 2. Create s3 bucket as image source and target
-        const destinationBucket = new StampImageBucket(this, 'Bucket', {
-            handler,
-        });
-
-        // 3. Initialise s3 bucket with needed folders and stamp image
-        new StampImageBucketDeployment(this, 'BucketDeployment', {
-            source: 'stamp-image',
-            destinationBucket,
         });
     }
 }
--- lib/stamp-image/index.ts
+++ lib/stamp-image/index.ts
@@ -1,5 +1,3 @@
-import { StampImageBucket } from './bucket';
-import { StampImageBucketDeployment } from './bucket-deployment';
 import { StampImageLamnbda } from './lambda';
 
-export { StampImageBucket, StampImageBucketDeployment, StampImageLamnbda };
+export { StampImageLamnbda };
