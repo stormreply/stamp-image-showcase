c478c064b2e940ed71a76f471a9b22ce25aac7cb
Henning Teek <h.teek@reply.de>
2020-04-26 22:31:06 +0200

    revert(stamp-image): removed lambda, s3 bucket and deployment for demo

--- lib/stamp-image/index.ts
+++
@@ -1,5 +0,0 @@
-import { StampImageBucket } from './bucket';
-import { StampImageBucketDeployment } from './bucket-deployment';
-import { StampImageLamnbda } from './lambda';
-
-export { StampImageBucket, StampImageBucketDeployment, StampImageLamnbda };
